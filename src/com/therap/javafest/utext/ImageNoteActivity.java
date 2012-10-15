ded502291e57b1d3038b7554b57092165c042e0c
Md. Fasihul Kabir (Rafi) <fkrafi@yahoo.com>
2012-10-16 02:00:16 +0600

    Reminder, Text, Image Note

--- src/com/therap/javafest/utext/ImageNoteActivity.java
+++ src/com/therap/javafest/utext/ImageNoteActivity.java
@@ -2,12 +2,33 @@ package com.therap.javafest.utext;
 
 import greendroid.app.GDActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ImageNoteActivity extends GDActivity {
+public class ImageNoteActivity extends GDActivity implements OnClickListener {
+
+	ImageView ivImage;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setActionBarContentView(R.layout.activity_image_note);
+
+		init();
+	}
+
+	private void init() {
+		ivImage = (ImageView) findViewById(R.id.ivImage);
+		ivImage.setOnClickListener(this);
+	}
+
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.ivImage:
+			Toast.makeText(this, "ImageView Clicked", Toast.LENGTH_LONG).show();
+			break;
+		}
 	}
 }
