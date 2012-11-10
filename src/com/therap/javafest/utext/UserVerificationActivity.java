0226279ee03e3c751ab57f7b48042c7a7e3d55af
Md. Fasihul Kabir (Rafi) <fkrafi@yahoo.com>
2012-11-11 00:50:53 +0600

    therap java fest

---
+++ src/com/therap/javafest/utext/UserVerificationActivity.java
@@ -0,0 +1,14 @@
+package com.therap.javafest.utext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserVerificationActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_verification);
+	}
+
+}
